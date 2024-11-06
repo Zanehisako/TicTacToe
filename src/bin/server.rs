2d66669d1f3a4bd5953948d312a86530c9853c77
Zanehisako <yassinetakiko@gmail.com>
2024-11-06 23:19:10 +0100

    new function to calculate min max surface

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,48 +1,47 @@
 use std::{
+    collections::HashSet,
     io::{Read, Write},
     net::TcpListener,
 };
 
-fn calculate_surface(i: u32, j: u32, k: u32) -> u32 {
-    return 2 * ((i * j) + (i * k) + (j * k));
+fn calculate_surface(i: i32, j: i32, k: i32) -> i32 {
+    2 * (i * j) + 2 * (i * k) + 2 * (j * k)
 }
 
-fn calcualte_surface_max_min() -> (u32, u32) {
-    print!("starting the function");
-    let mut combinations = [0; 1000];
-    for i in 1..1000 {
-        print!("i: {:?}", i);
+fn calculate_surface_max_min(n: i32) -> String {
+    let mut combinations = HashSet::new();
+
+    for i in 1..=n {
         let mut j = 1;
-        loop {
-            let k = 1000 / (i * j);
-            if (i * j * k) == 1000 {
+        while j * i <= n {
+            let k = (n / (i * j)) as i32;
+            if i * j * k == n {
                 let surface = calculate_surface(i, j, k);
-                combinations[i as usize] = surface;
-                if j > 1000 - 1 - i {
-                    break;
-                } else {
-                    j += 1;
-                }
+                combinations.insert(surface);
             }
+            j += 1;
         }
     }
-    return (
-        *combinations.iter().min().unwrap(),
-        *combinations.iter().max().unwrap(),
-    );
+
+    let max_surface = combinations.iter().max().unwrap_or(&0);
+    let min_surface = combinations.iter().min().unwrap_or(&0);
+
+    format!(
+        "Minimum surface area: {}\nMaximum surface area: {}",
+        min_surface, max_surface
+    )
 }
+
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:8080").unwrap();
-    let (min, _) = calcualte_surface_max_min();
-    println!("the min is : {:?}", min);
     loop {
         let mut buf = [0; 1024];
         let (mut socket, addr) = listner.accept().unwrap();
         println!("client {:?}", addr);
         let message = socket.read(&mut buf);
         println!("{:?}", message);
-        let (min, _) = calcualte_surface_max_min();
-        println!("the min is : {:?}", min);
-        socket.write(&min.to_le_bytes()).unwrap();
+        let result = calculate_surface_max_min(10000000);
+        socket.write(&result.as_bytes()).unwrap();
+        println!("the min is : {:?}", result);
     }
 }
